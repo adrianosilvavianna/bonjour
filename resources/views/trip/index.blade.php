c436f1d7aef3fbb038a3eb2ae32ef881d566845c
Adriano <adriano_vianna@outlook.com>
2017-09-26 17:49:55 -0300

    fix

--- resources/views/trip/index.blade.php
+++ resources/views/trip/index.blade.php
@@ -1,7 +1,43 @@
 @extends('layouts.app')
 
 @section('content')
+
+    <style>
+        .card img {
+            width: 30%;
+            height: auto;
+            padding: 5%;
+        }
+    </style>
+
 <div class="container-fluid">
+    <div class="row">
+        <div class="col-lg-12 col-md-12 col-sm-12">
+            <div class="card card-stats">
+
+                <div class="card-content">
+                    <img class="pull-left" src="../assets/img/faces/marc.jpg" style="border-radius: 50%" >
+                    <p class="category">Com : Nome do cara</p>
+                    <h3 class="title">26/09/2017</h3>
+                    <strong>De :</strong> Rua Solimoes, 849 - Curitiba <br/>
+                    <strong>Para :</strong> Rua Cidade De Tubarão, 849 - Curitiba
+                </div>
+                <div class="card-footer">
+                    <div class="stats">
+                         Avaliação : <i class="material-icons">star_rate</i>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
+
+@section('scripts')
+
+@show
+
+<!-- <div class="container-fluid">
     <div class="row">
         <div class="col-md-12">
             <div class="card">
@@ -47,8 +83,7 @@
         </div>
     </div>
 </div>
-@endsection
 
-@section('scripts')
+-->
 
-@show
+
