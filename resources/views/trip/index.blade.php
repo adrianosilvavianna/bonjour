696b7fb0d1ed5ed95a0234e491ccc0baede1ea8e
Adriano <adriano_vianna@outlook.com>
2017-10-06 13:45:26 -0300

    fix

--- resources/views/trip/index.blade.php
+++ resources/views/trip/index.blade.php
@@ -4,9 +4,9 @@
 
     <style>
         .card img {
-            width: 20%;
+            width: 10%;
             height: auto;
-            padding: 5%;
+            margin-bottom: 1%;
         }
         p{
             height: auto;
@@ -19,14 +19,10 @@
             <div class="card card-stats">
                 <div class="card-content">
                     <img class="pull-left" src="../assets/img/faces/marc.jpg" style="border-radius: 50%" >
-                    <h3 class="title">26/09/2017</h3>
+                    <h3 class="pull-left" style="margin-left: 5%">Rogerio Silveira Pinto</h3>
+                    <h3 class="title">26/09/2017 ás 13:30</h3><br>
                     <strong>De :</strong> Rua Solimoes, 849 - Curitiba <br/>
                     <strong>Para :</strong> Rua Cidade De Tubarão, 849 - Curitiba
-                </div>
-                <div class="card-footer">
-                    <div class="stats">
-                         Avaliação :
-                    </div>
                 </div>
             </div>
         </div>
