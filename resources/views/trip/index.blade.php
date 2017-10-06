6a2816fae0d7f68fac1a01c261e12ba5c73260cf
Adriano <adriano_vianna@outlook.com>
2017-10-06 14:02:36 -0300

    fix

--- resources/views/trip/index.blade.php
+++ resources/views/trip/index.blade.php
@@ -20,14 +20,18 @@
                 <div class="card-content">
                     <img class="pull-left" src="../assets/img/faces/marc.jpg" style="border-radius: 50%" >
                     <h3 class="pull-left" style="margin-left: 5%">Rogerio Silveira Pinto</h3>
+
                     <h3 class="title">26/09/2017 ás 13:30</h3><br>
                     <strong>De :</strong> Rua Solimoes, 849 - Curitiba <br/>
-                    <strong>Para :</strong> Rua Cidade De Tubarão, 849 - Curitiba
+                    <strong>Para :</strong> Rua Cidade De Tubarão, 849 - Curitiba <br>
+                    <a href="#" class="btn btn-primary ">Pegar Carona</a>
                 </div>
             </div>
         </div>
     </div>
 </div>
+
+
 @endsection
 
 @section('scripts')
