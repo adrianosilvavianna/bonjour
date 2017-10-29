3b5379a08f30d4ecfe5f31f31c7bfc7eecf15fbf
Adriano <adriano_vianna@outlook.com>
2017-10-29 14:49:11 -0200

    fix

--- resources/views/trip/_inputs.blade.php
+++ resources/views/trip/_inputs.blade.php
@@ -2,7 +2,12 @@
     <div class="col-md-4 {{ $errors->has('date') ? ' has-error' : '' }}">
         <div class="form-group  ">
             <label class="control-label">Data de partida</label>
-            <input type="date" class="form-control" name="date" min="{{ date('Y-m-d') }}" id="date" value="{{ $trip->date }}">
+
+            @if(!empty($trip))
+                <input type="date" class="form-control" name="date" min="{{ date('Y-m-d') }}" id="date" value="{{ $trip->date }}">
+            @else
+                <input type="date" class="form-control" name="date" min="{{ date('Y-m-d') }}" id="date" value="">
+            @endif
         </div>
         @if ($errors->has('date'))
             <span class="help-block">
@@ -14,7 +19,11 @@
     <div class="col-md-2 {{ $errors->has('time') ? ' has-error' : '' }}">
         <div class="form-group  ">
             <label class="control-label">Hora de partida</label>
-            <input type="time" class="form-control" name="time" id="time" value="{{ $trip->time }}">
+            @if(!empty($trip))
+                <input type="time" class="form-control" name="time" id="time" value="{{ $trip->time }}">
+            @else
+                <input type="time" class="form-control" name="time" id="time" value="">
+            @endif
         </div>
         @if ($errors->has('time'))
             <span class="help-block">
@@ -26,12 +35,16 @@
     <div class="col-md-4 {{ $errors->has('vehicle_id') ? ' has-error' : '' }}">
         <div class="form-group  ">
             <label class="control-label">Veículo</label>
-            <select type="text" name="vehicle_id" class="form-control" >
-                <option value="{{ $trip->Vehicle->id }}">Marca: {{ $trip->Vehicle->brand }} || Modelo: {{ $trip->Vehicle->model }}</option>
-                @foreach($vehicles as $vehicle)
-                    <option value="{{ $vehicle->id }}">Marca: {{ $vehicle->brand }} || Modelo: {{ $vehicle->model }}</option>
-                @endforeach
-            </select>
+
+                <select type="text" name="vehicle_id" class="form-control" >
+                    @if(!empty($trip))
+                        <option value="{{ $trip->Vehicle->id }}">Marca: {{ $trip->Vehicle->brand }} || Modelo: {{ $trip->Vehicle->model }}</option>
+                    @endif
+                    @foreach($vehicles as $vehicle)
+                        <option value="{{ $vehicle->id }}">Marca: {{ $vehicle->brand }} || Modelo: {{ $vehicle->model }}</option>
+                    @endforeach
+                </select>
+
         </div>
         @if ($errors->has('vehicle_id'))
             <span class="help-block">
