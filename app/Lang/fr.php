b5eaa6ed9d410f1857754c1575642b32a5f3cc31
afornalimatheus <afornalimatheus@gmail.com>
2017-11-14 14:44:46 -0200

    'ajustesTradução'

--- app/Lang/fr.php
+++ app/Lang/fr.php
@@ -3,5 +3,39 @@
  * Created by PhpStorm.
  * User: Adriano
  * Date: 13/11/2017
- * Time: 19:27
+ * Time: 19:26
  */
+
+//Itens do menu ao lado esquerdo.
+
+define('nomeProjeto', 'Bonjou');
+define('oProjeto', 'Le projet Bonjou');  //sobreProjeto
+define('ajudaEinformacoes', 'Help and useful information'); //ajudaInformacao
+define('contato', 'Contact');
+
+//Itens principais da tela inicial.
+
+define('significadoBonjou', "Le projet Bonjou, qui signifie «bonjour» en dialecte créole, a pour objectif d'aider à résoudre le problème du décrochage scolaire en raison d'un manque de moyens de transport, ainsi que de promouvoir le rapprochement entre Brésiliens et Haïtiens.");
+define('btnLogin', 'Login');
+define('btnCadastrarse', 'Inscrivez-vous');
+define('btnEntrar', 'Connexion');
+
+define('objetivoProjeto1', "Le projet vise l'intégration et le covoiturage parmi les réfugiés, les migrants de toutes les nationalités et les habitants de Curitiba.");
+define('objetivoProjeto2', 'Un système Web qui peut demander et offrir des auto-stoppeurs peut être programmé auparavant, en aidant les réfugiés et les migrants.');
+define('objetivoProjeto3', "Système créé en conjonction avec le cours d'analyse et de développement des systèmes de l'Université Tuiuti de Paraná.");
+
+define('planetaUmSo', "Project The Planet est l'un.");
+define('sobrePlanetaUmSo', "Le projet fonctionne dans un format de réunions visant à intégrer les réfugiés et les migrants de tous les horizons avec des personnes de Curitiba qui veulent s'engager dans l'inclusion de ce groupe dans la vie de la ville.");
+
+define('localEncontros', 'Lieu des réunions');
+
+define('ajudaInfos1', 'Aide et informations utiles.');
+define('ajudaInfos2', 'Emplacement des écoles, lieux de production des documents nécessaires, lieux de soutien aux immigrants et contacts utiles.');
+
+define('escrevaSuaDuvida', "Écrivez votre question, votre question ou écrivez un message demandant d'être un partisan du projet La planète est une.");
+
+define('nome', 'Nom');
+define('email', 'E-mail');
+define('mensagem', 'Message');
+define('btnEnviarMsg', 'Envoyer message');
+
--- app/Lang/pt-br.php
+++ app/Lang/pt-br.php
@@ -10,8 +10,8 @@
 
 define('nomeProjeto', 'Bonjou');
 define('oProjeto', 'O Projeto Bonjou');  //sobreProjeto
-define('ajudaEinformacoes', 'Ajuda e informações úteis'); //ajudaInformacao
-define('contato', 'Contato');
+define('ajudaEinformacoes', 'Aide et informations utiles'); //ajudaInformacao
+define('contato', 'Contactez nous');
 
 //Itens principais da tela inicial.
 
