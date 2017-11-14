0f8656fe2f4892486049ef379ab23db607d74d82
afornalimatheus <afornalimatheus@gmail.com>
2017-11-14 14:01:18 -0200

    'traduçãoInglês'

--- app/Lang/en.php
+++ app/Lang/en.php
@@ -1,14 +1,41 @@
 <?php
-//ITENS DO MENU LATERAL ESQUERDO
+/**
+ * Created by PhpStorm.
+ * User: Adriano
+ * Date: 13/11/2017
+ * Time: 19:26
+ */
+
+//Itens do menu ao lado esquerdo.
+
 define('nomeProjeto', 'Bonjou');
-define('oProjeto', 'The Project Bonjou');  //sobreProjeto
-define('ajudaEinformacoes', 'Helper and useful information'); //ajudaInformacao
+define('oProjeto', 'The Bonjou Project');  //sobreProjeto
+define('ajudaEinformacoes', 'Help and useful information'); //ajudaInformacao
 define('contato', 'Contact');
 
 //Itens principais da tela inicial.
 
-define('significadoBonjou', '
-The Bonjou project, which means "Good Morning" in Creole dialect, aims to help in the issue of strong school dropout due to the lack of means of transportation, as well as promoting the rapprochement between Brazilians and Haitians.');
+define('significadoBonjou', 'The Bonjou project, which means "Good Morning" in Creole dialect, to help in the dissemination of the strong school dropout, due to the lack of means of transport, besides promoting a rapprochement between Brazilians and Haitians.');
 define('btnLogin', 'Login');
-define('btnCadastrarse', 'Register'); //btnCadasrar
-define('btnEntrar', 'Sign');
+define('btnCadastrarse', 'Register');
+define('btnEntrar', 'Get in');
+
+define('objetivoProjeto1', 'The project aims at integration and carpooling among refugees, migrants of all nationalities and people of Curitiba.');
+define('objetivoProjeto2', 'Web system that can request and offer hitchhikers can be scheduled previously, helping the refugees and migrants.');
+define('objetivoProjeto3', 'System created in conjunction with the course of Analysis and Development of Systems of the Tuiuti University of Paraná.');
+
+define('planetaUmSo', 'Project The Planet is One.');
+define('sobrePlanetaUmSo', 'The project works in a format of meetings aimed at integrating refugees and migrants from all walks of life with people from curitiba who want to engage in the inclusion of this group in the life of the city.');
+
+define('localEncontros', 'Location of meetings');
+
+define('ajudaInfos1', 'Help and useful information.');
+define('ajudaInfos2', 'Location of schools, places to make necessary documents, places to support immigrants, and useful contacts.');
+
+define('escrevaSuaDuvida', 'Write your question, your question, or write a message asking to be a supporter of the project The Planet is One.');
+
+define('nome', 'Name');
+define('email', 'E-mail');
+define('mensagem', 'Message');
+define('btnEnviarMsg', 'Send message');
+
