2542eb319412c6432d51f7fa27c82799e04ee229
adrianosilvavianna <adriano_vianna@outlook.com>
2018-04-25 11:27:46 -0300

    fix autoload aplication

--- app/Service/Application.php
+++ app/Service/Application.php
@@ -7,6 +7,6 @@ class Application extends \Illuminate\Foundation\Application
 
     public function publicPath()
     {
-        return dd($this->basePath.DIRECTORY_SEPARATOR.'../public');
+        return $this->basePath.DIRECTORY_SEPARATOR.'../public';
     }
 }
